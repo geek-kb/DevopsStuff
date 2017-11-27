324f61ba8f76f8e01d8a092532deb752bf77e1e8
Itai Ganot <itaig@tikalk.com>
2017-11-27 13:04:56 +0200

    Adds description to script

--- Scripts/aws/aws-delete_old_vpc_associations.sh
+++ Scripts/aws/aws-delete_old_vpc_associations.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -e
+# This shell script is intended to run as a Jenkins job which checks current available VPC's against VPC's associated to the specified Route53 hosted zone.
 crvpc_arr=()
 asvpc_arr=()
-region=""
+region="us-west-2"
 hostedzoneid="HOSTEDZONEID"
 for currentvpc in $(aws ec2 describe-vpcs --region $region| grep VpcId | awk '{print $2}' | tr -d '\"|,'); do crvpc_arr+=($currentvpc); done
 for asvpc in $(aws route53 get-hosted-zone --id $hostedzoneid --region $region| grep VPCId | awk '{print $2}' | tr -d '\"|,'); do asvpc_arr+=($asvpc); done
