38030bd952927abcb3082bfe5c8225744d600e54
Itai Ganot <itaig@tikalk.com>
2017-12-11 13:30:06 +0200

    Gets all db snapshots in a specified region

--- Scripts/jenkins/populate_parameter_with_latest_rds_snapshotid.groovy
+++ Scripts/jenkins/populate_parameter_with_latest_rds_snapshotid.groovy
@@ -2,16 +2,14 @@
 # Returns an array of latest available RDS Snapshot Ids per environment [ EnvDBNames ].
 # Can be used to populate a parameter in Jenkins + Extended Parameter plugin.
 # Script by Itai Ganot 2017
+region = 'eu-west-1'
 String todayDate = new Date().format( 'yyyy-MM-dd' )
-def EnvDBNames = ['dev-rds', 'stg-rds', 'prd-rds']
 retval=[]
-EnvDBNames.each {
-	def rdbs = "aws rds describe-db-snapshots --db-instance-identifier ${it} --snapshot-type automated --query DBSnapshots[?SnapshotCreateTime>='$todayDate'].DBSnapshotIdentifier".execute().text.eachLine {
-		if (!it.contains(']') && (!it.contains('[') ) )
-			retval.add(it.replaceAll('\"','').trim())
-	}
+def rdbs = "aws rds describe-db-snapshots --snapshot-type manual --query DBSnapshots[?SnapshotCreateTime>='$todayDate'].DBSnapshotIdentifier --region $region".execute().text.eachLine {
+  if (!it.contains(']') && (!it.contains('[') ) )
+    retval.add(it.replaceAll('\"','').trim())
 }
 
 retval.each {
-	println it
+   println it
 }
