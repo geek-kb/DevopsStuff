41c1c4e3cc9147e38439c98c5a9f209436e3d7c1
Itai Ganot <itaig@tikalk.com>
2017-12-11 14:39:11 +0200

    Fixes a typo

--- Scripts/jenkins/populate_parameter_with_used_cidrs.groovy
+++ Scripts/jenkins/populate_parameter_with_used_cidrs.groovy
@@ -1,5 +1,5 @@
 #!/usr/local/bin/groovy
-# Returns an array of current used CIDR blocks to avoid same cidr in two environments. 
+# Returns an array of current used CIDR blocks to avoid same CIDR block in an environments. 
 # Can be used to populate a parameter in Jenkins + Extended Parameter plugin.
 # Script by Itai Ganot 2017
 def p = ['/usr/local/bin/aws', 'ec2', 'describe-vpcs'].execute() | 'grep -w CidrBlock'.execute() | ['awk', '{print $2}'].execute() | ['tr', '-d', '"\\"\\|,\\|\\{\\|\\\\["'].execute() | 'uniq'.execute()
