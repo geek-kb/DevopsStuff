271d3c0be5021af1d706f7fe88d82b6c476ca5de
Itai Ganot <itaig@tikalk.com>
2017-12-12 22:44:53 +0200

    Adds support for multiple regions

--- Scripts/jenkins/populate_parameter_with_used_cidrs.groovy
+++ Scripts/jenkins/populate_parameter_with_used_cidrs.groovy
@@ -1,14 +1,16 @@
 #!/usr/local/bin/groovy
-# Returns an array of current used CIDR blocks to avoid same CIDR block in an environments. 
-# Can be used to populate a parameter in Jenkins + Extended Parameter plugin.
-# Script by Itai Ganot 2017
-def p = ['/usr/local/bin/aws', 'ec2', 'describe-vpcs'].execute() | 'grep -w CidrBlock'.execute() | ['awk', '{print $2}'].execute() | ['tr', '-d', '"\\"\\|,\\|\\{\\|\\\\["'].execute() | 'uniq'.execute()
-p.waitFor()
-def output = []
-p.text.eachLine { line ->
-        output << line
-}
-
-output.each {
-        println it
-}
+// Returns an array of current used CIDR blocks to avoid same CIDR block in an environments. 
+// Can be used to populate a parameter in Jenkins + Extended Parameter plugin.
+//  Script by Itai Ganot 2017
+def regions = ['us-west-2', 'us-east-1', 'eu-west-1']
+        def output = []
+        regions.each { region ->
+            def p = ['/usr/local/bin/aws', 'ec2', 'describe-vpcs', '--region', region].execute() | 'grep -w CidrBlock'.execute() | ['awk', '{print $2}'].execute() | ['tr', '-d', '"\\"\\|,\\|\\{\\|\\\\["'].execute() | 'uniq'.execute()
+            p.waitFor()
+            p.text.eachLine { line ->
+                output << line
+            }
+        }
+        output.each {
+            println it
+        }
