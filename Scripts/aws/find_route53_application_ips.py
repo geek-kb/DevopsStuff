e0b6cb2cd88eff946a867704f351009c07bd1258
Itai Ganot <itaig@tikalk.com>
2017-12-14 17:41:52 +0200

    Regex to find can now be set into a variable

--- Scripts/aws/find_route53_application_ips.py
+++ Scripts/aws/find_route53_application_ips.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
+# This script receives a Hosted Zone Id as parameter and returns the IPs of the servers that match the regex in variable "reg".
+# Itai Ganot 2017
 import boto3
 import re, sys
+
+reg = 'api|analytics|batch'
 
 if len(sys.argv) < 2:
     print('Please supply Hosted Zone Id')
@@ -14,14 +18,11 @@ r53client = boto3.client('route53')
 
 response = r53client.list_resource_record_sets(
         HostedZoneId=hostedzoneid,
-        StartRecordName='dev',
-        StartRecordType='A'
         )
 
 for resource in response['ResourceRecordSets']:
   records = resource['ResourceRecords'][0]
   if 'Name' in resource:
-    name = re.findall('api|batch|analytics', resource['Name'])
+    name = re.findall(reg, resource['Name'])
     if name:
       print records['Value']
-      #print records['Value'], name[0]
