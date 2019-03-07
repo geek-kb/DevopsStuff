49ae26620ffad87830a031a05d99d3dbb2ca98a5
itai ganot <camelel@gmail.com>
2019-03-07 18:14:31 +0200

    Adds getopts to script

--- Scripts/aws/aws-find_ssm_param_key_by_param_value.sh
+++ Scripts/aws/aws-find_ssm_param_key_by_param_value.sh
@@ -14,31 +14,55 @@
 # Script by Itai Ganot 2019
 
 function usage(){
-  echo "Usage: ${basedir}${0} parameter_value_to_find parameters_list"
+  echo "Usage: ${basedir}${0} -p parameter_value_to_find -l parameter_list"
+  echo " "
+  echo "Optional:                                                         "
+  echo "-d                                                        [debug] "
 }
 
-if [[ $# -lt "2" ]]; then
+if [[ $# -lt 4 ]]; then
   usage
   exit
 fi
 
+debug=false
+
+while getopts ":p:l:d" opt; do
+  case ${opt} in
+    p)
+      value_to_find=${OPTARG}
+      ;;
+    l)
+      param_list=${OPTARG}
+      ;;
+    d)
+      debug=true
+      ;;
+    \?)
+      echo "Unknown option: -$OPTARG" >&2; exit 1
+      ;;
+  esac
+done
+
 GREEN=$(tput setaf 2)
 CYAN=$(tput setaf 6)
 NOCOLOR=$(tput sgr0)
-value_to_find=$1
-param_list=$2
 cwd=$(pwd)
 date_time=$(date +'%d-%m-%Y_%T' | tr ":" "-")
 log=${cwd}/${value_to_find}-${date_time}.log
 counter="0"
-touch $log
+pid=$$
+
+#touch ${log}
+echo "Pid: ${pid}"
 
 for param in $(cat ${param_list}); do
   check_key=$(aws ssm get-parameters --with-decryption --names "${param}" | jq -r ".Parameters[] | .Value")
   if [[ ${check_key} = ${value_to_find} ]]; then
     echo "Value ${value_to_find} found in parameter ${param}" | tee -a ${log}
   fi
-  counter=$(expr ${counter} + 1)
-  echo -e "#${counter}: Finished processing {${CYAN}Key${NOCOLOR}:${param},${GREEN}Value${NOCOLOR}:${check_key}}"
+  if ${debug}; then
+    counter=$(expr ${counter} + 1)
+    echo -e "#${counter}: Finished processing {${CYAN}Key${NOCOLOR}:${param},${GREEN}Value${NOCOLOR}:${check_key}}"
+  fi
 done
-
