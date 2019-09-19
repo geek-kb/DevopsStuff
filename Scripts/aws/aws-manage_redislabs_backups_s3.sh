06fabcd007a82e4fa44d9f09de98fc90da163b40
Itai Ganot <itai.ga@moonactive.com>
2019-09-19 11:58:54 +0300

    Updates script

--- Scripts/aws/aws-manage_redislabs_backups_s3.sh
+++ Scripts/aws/aws-manage_redislabs_backups_s3.sh
@@ -4,21 +4,19 @@
 # hour per those days and deleting the rest of the backups of each day.
 # The script matches the following naming convension:
 # bk20190903-150002-2-CompanyDB-NEW-54_of_100-208-8684-8846.rdb.gz
-# This script uses a tool called ddiff which can be downloaded from here:
-# https://bitbucket.org/hroptatyr/dateutils/downloads/dateutils-0.4.6.tar.xz
 
 todayDate=$(date +'%Y-%m-%d')
 runTime=$(date +'%H:%M:%S')
 
 function usage(){
-  yellow  "-----------------------------------------------------------------------"
-  yellow  "# Usage: ${basename}${0} -b bucket_name -t chosen_backup_time    #"
-  yellow  "# Available redis database bucket names:                #"
+  echo  "-----------------------------------------------------------------------"
+  echo  "# Usage: ${basename}${0} -b bucket_name -t chosen_backup_time    #"
+  echo  "# Available redis database bucket names:                #"
   echo    "# company-redis-dr                                   #"
   echo    "# company-redis-enterprise-backup                    #"
-  yellow  "# Chosen backup times: "
+  echo  "# Chosen backup times: "
   echo    "# Any hour within 00 and 23 (2 digits)                  #"
-  yellow  "---------------------------------------------------------"
+  echo  "---------------------------------------------------------"
 }
 
 RED=`tput setaf 1`
@@ -43,21 +41,42 @@ function purple {
   echo -e -n "${PURPLE}$* $NOCOLOR\n"
 }
 
+function scriptRequirements(){
+  which ddiff &> /dev/null
+  if [[ ! $? -eq 0 ]]; then
+    echo "ddiff command not found, installing..."
+    cd /tmp
+    wget https://bitbucket.org/hroptatyr/dateutils/downloads/dateutils-0.4.6.tar.xz
+    tar xvf dateutils-0.4.6.tar.xz
+    cd dateutils-0.4.6
+    ./configure
+    make
+    sudo cp src/ddiff /usr/bin/
+    rm -rf /tmp/dateutils-0.4.6.tar.xz /tmp/dateutils-0.4.6
+  fi
+}
+
 function createDbList(){
-  green "Creating list of databases in the bucket, this could take up to 15 minutes so be patient..."
+  echo "Creating list of databases in the bucket, this could take up to 15 minutes so be patient..."
   dbList=$(aws s3 ls s3://$bucketName | grep PRE | awk '{print $2}' | tr -d '\/' | sed -e '/^$/d')
 }
 
 function createFileList(){
   case $database in
     'RedisBackUps')
-    green "Processing files for database C45..."
+    echo "Processing files for database C45..."
+    database='RedisBackUps'
+    ;;
+    'redisbackups')
+    echo "Processing files for database C45..."
+    database='RedisBackUps'
     ;;
     'tier-3')
-    green "Processing files for database ROF2..."
+    echo "Processing files for database ROF2..."
+    database='tier-3'
     ;;
     *)
-    green "Processing files for database ${database}..."
+    echo "Processing files for database ${database}..."
     ;;
   esac
   aws s3 ls s3://${bucketName}/${database}/ > /tmp/${database}
@@ -74,12 +93,21 @@ function cutDate(){
 
 function findFilesToDelete(){
   if [[ $dateDiff -gt 14 && $dateDiff -lt 90 ]]; then
-    yellow "Date difference is between 14 and 90 days!"
+    echo "Date difference is between 14 and 90 days!"
     if [[ ! $line =~ ^bk${somedate}-${chosenBackupHour}.* ]]; then
-      red "deleting file $line"
-      red "COMMAND: aws s3 rm s3://${bucketName}/${database}/${line}"
+      echo "Deleting file $line"
+      aws s3 rm s3://${bucketName}/${database}/${line}
+      lineDeleteMarkerVersionIds=$(aws s3api list-object-versions --bucket ${bucketName} --prefix ${database}/${line} --output json --query 'DeleteMarkers[].VersionId' | jq -r '.[]')
+      echo "Deleting all versions of file ${line}"
+      for delMarkerVerId in $(echo $lineDeleteMarkerVersionIds); do
+        echo "Deleting DeleteMarker of file ${line} with VersionId ${delMarkerVerId}"
+        aws s3api delete-object --bucket ${bucketName} --key ${database}/${line} --version-id ${delMarkerVerId}
+      done
+      echo "Deleting Latest version of file ${line}"
+      latestVersion=$(aws s3api list-object-versions --bucket ${bucketName} --prefix ${database}/${line} --output json --query 'Versions[].VersionId' | jq -r '.[]')
+      aws s3api delete-object --bucket ${bucketName} --key ${database}/${line} --version-id ${latestVersion}
     else
-      green "Filename $line contains selected ${chosenHour} so not deleting!"
+      echo "Filename $line contains selected ${chosenHour} so not deleting!"
     fi
   fi
 }
@@ -89,9 +117,12 @@ function dateDiff(){
   bkMonth=$(echo ${somedate:4:2})
   bkDay=$(echo ${somedate:8-2})
   bkpDate=${bkYear}-${bkMonth}-${bkDay}
-  yellow ---------------------------------------------------------------------------------
+  echo ---------------------------------------------------------------------------------
   case $database in
     'RedisBackUps')
+    echo "Database name: C45"
+    ;;
+    'redisbackups')
     echo "Database name: C45"
     ;;
     'tier-3')
@@ -106,7 +137,7 @@ function dateDiff(){
   echo "Today Date: $todayDate"
   echo "Backup Date: $bkpDate"
   dateDiff=$(ddiff $todayDate $bkpDate | tr -d '-')
-  purple "DateDiff: $dateDiff"
+  echo "DateDiff: $dateDiff"
 }
 
 function cleanTraces(){
@@ -134,12 +165,15 @@ while getopts "b:t:" opt; do
   esac
 done
 
-green "Script has started running at: ${runTime}"
-#createDbList
-#for database in $(echo $dbList); do
-for database in $(cat /Users/itaiganot/src/company/devops/scripts/222); do
-  if [[ $database = 'RedisBackUps' ]]; then
+echo "Script has started running at: ${runTime}"
+scriptRequirements
+createDbList
+for database in $(echo $dbList |  tr "[:upper:]" "[:lower:]"); do
+#for database in $(cat /Users/itaiganot/src/company/devops/scripts/222 |  tr "[:upper:]" "[:lower:]"); do
+  if [[ $database = 'RedisBackUps' || $database = 'redisbackups' ]]; then
     chosenHour="${chosenBackupHour}0002"
+  else
+    chosenHour="${chosenBackupHour}0000"
   fi
   createFileList
   for line in $(tac /tmp/${database}.txt ); do
@@ -147,5 +181,5 @@ for database in $(cat /Users/itaiganot/src/company/devops/scripts/222); do
     dateDiff
     findFilesToDelete
   done
+  cleanTraces
 done
-cleanTraces
