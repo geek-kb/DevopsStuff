f053df82803a9da7defd98fc9f85d91ca9dfaaea
Itai Ganot <camelel@gmail.com>
2021-07-26 12:39:23 +0300

    Updates aws_transfer-create_new_customer_directory_sftp.sh

--- Scripts/aws/aws_transfer-create_new_customer_directory_sftp.sh
+++ Scripts/aws/aws_transfer-create_new_customer_directory_sftp.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# Written by Itai Ganot 2021
 
 server_id='s-XXXXXXXXXX'
-s3_bucket_name='a-bucket-name'
+s3_bucket_name='company-cs-ftp'
 whitelist_ips_sg_id='sg-XXXXXXXXXX'
-username=${USERNAME}
+customer_directory_name=${CUSTOMER_DIRECTORY_NAME}
 
 if [[ -z ${USER_PUBLIC_SSH_KEY} ]]; then
     existing_pub_ssh_key='false'
@@ -27,27 +26,17 @@ function get_jq(){
 	cp jq /usr/bin/
 }
 
-# if [[ $# -ne 2 ]]; then
-#     usage
-#     echo "No username supplied! exiting!"
-#     exit 1
-# fi
-
-# while getopts "u:" opt; do
-#     case $opt in
-#         u)
-#         username=${OPTARG}
-#         ;;
-#         *)
-#         usage
-#         ;;
-#     esac
-# done
-
 # Test if jq command line tool is installed (required) - if not, install it
 which jq >/dev/null
 if [[ $? -ne 0 ]]; then
   get_jq
+fi
+
+dir_name=$(aws transfer list-users --server-id ${server_id} | jq -r --arg dirname ${customer_directory_name} '.Users[] | select(.HomeDirectory | contains($dirname)) .HomeDirectory' | cut -d"/" -f3)
+if [[ ${dir_name} == ${customer_directory_name} ]]; then
+    echo "Customer directory name \"${customer_directory_name}\" already exists!"
+    echo "Exiting!"
+    exit 1
 fi
 
 # AWS Iam - create AWS Transfer assume role policy to attach to the role
@@ -67,10 +56,10 @@ cat <<EOF > transfer-assume-role-policy
 }
 EOF
 
-sed -i.bak "s/CSFTPUSER/$username/g" transfer-assume-role-policy
+sed -i.bak "s/CSFTPUSER/$customer_directory_name/g" transfer-assume-role-policy
 
 # AWS Iam - create role and attach the above policy to it
-aws iam create-role --role-name ${s3_bucket_name}-read-write-${username} --assume-role-policy-document file://transfer-assume-role-policy
+aws iam create-role --role-name ${s3_bucket_name}-read-write-${customer_directory_name} --assume-role-policy-document file://transfer-assume-role-policy
 rm -f transfer-assume-role-policy
 
 # AWS Iam - create policy to allow user actions on the s3 bucket
@@ -104,47 +93,48 @@ cat <<EOF > temp-${s3_bucket_name}-read-write
     ]
 }
 EOF
-sed -i.bak "s/USERNAME/$username/g" temp-company-cs-ftp-read-write
+sed -i.bak "s/USERNAME/$customer_directory_name/g" temp-company-cs-ftp-read-write
 sed -i.bak "s/S3BUCKETNAME/$s3_bucket_name/g" temp-company-cs-ftp-read-write
-aws iam create-policy --policy-name ${s3_bucket_name}-read-write-${username} --policy-document file://temp-company-cs-ftp-read-write
+aws iam create-policy --policy-name ${s3_bucket_name}-read-write-${customer_directory_name} --policy-document file://temp-company-cs-ftp-read-write
 rm -f temp-${s3_bucket_name}-read-write
 
 # AWS Iam - attach the below policies to the new role
-policies="company-cs-ftp-scope-down company-cs-ftp-deny-mkdir company-cs-ftp-read-write-${username}"
+policies="company-cs-ftp-scope-down company-cs-ftp-deny-mkdir company-cs-ftp-read-write-${customer_directory_name}"
 for policy_name in ${policies}; do
     arn=$(aws iam list-policies --scope Local --output json | jq -r --arg polname ${policy_name} '.Policies[] | select(.PolicyName==$polname) | .Arn')
-    echo "Adding ${policy_name} - ${arn} to newly created role company-cs-ftp-read-write-${username}"
-    aws iam attach-role-policy --role-name company-cs-ftp-read-write-${username} --policy-arn $arn
+    echo "Adding ${policy_name} - ${arn} to newly created role company-cs-ftp-read-write-${customer_directory_name}"
+    aws iam attach-role-policy --role-name company-cs-ftp-read-write-${customer_directory_name} --policy-arn $arn
 done
 
 # AWS Iam - verify the role has been created successfully
-aws iam get-role --role-name company-cs-ftp-read-write-${username}
+aws iam get-role --role-name company-cs-ftp-read-write-${customer_directory_name}
 
 if [[ $? -eq 0 ]]; then
-    echo "Role company-cs-ftp-read-write-${username} successfully created!"
-    role_arn=$(aws iam get-role --role-name company-cs-ftp-read-write-${username} | jq -r '.Role.Arn')
+    echo "Role company-cs-ftp-read-write-${customer_directory_name} successfully created!"
+    role_arn=$(aws iam get-role --role-name company-cs-ftp-read-write-${customer_directory_name} | jq -r '.Role.Arn')
 fi
 
 # AWS S3 - create a folder with the name of the user in the bucket
-aws s3api put-object --bucket ${s3_bucket_name} --key ${username}/
+aws s3api put-object --bucket ${s3_bucket_name} --key ${customer_directory_name}/
 
 # AWS Transfer - creates the user and attaches the role to it
 # If the version of AWS-CLI is 2.x then the command should be:
-#aws transfer create-user --home-directory /company-cs-ftp/${username} --home-directory-type PATH --role ${role_arn} --server-id ${server_id} --user-name ${username}
+#aws transfer create-user --home-directory /company-cs-ftp/${customer_directory_name} --home-directory-type PATH --role ${role_arn} --server-id ${server_id} --user-name ${customer_directory_name}
 # But for version 1.x the command should be:
-aws transfer create-user --home-directory /${s3_bucket_name}/${username} --role ${role_arn} --server-id ${server_id} --user-name ${username}
+aws transfer create-user --home-directory /${s3_bucket_name}/${customer_directory_name} --role ${role_arn} --server-id ${server_id} --user-name ${customer_directory_name}
 
 if [[ $? -eq 0 ]]; then
-    echo "The following directory has been created for user ${username}:"
-    aws transfer describe-user --server-id ${server_id} --user-name ${username}
+    echo "The following directory has been created for user ${customer_directory_name}:"
+    aws transfer describe-user --server-id ${server_id} --user-name ${customer_directory_name}
 fi
 
 # If a public ssh key has been provided, add it to the user
 if [[ ${existing_pub_ssh_key} == 'true' ]]; then
-    aws transfer import-ssh-public-key --server-id ${server_id} --ssh-public-key-body "${USER_PUBLIC_SSH_KEY}" --user-name ${username}
+    aws transfer import-ssh-public-key --server-id ${server_id} --ssh-public-key-body "${USER_PUBLIC_SSH_KEY}" --user-name ${customer_directory_name}
     if [[ $? -eq 0 ]]; then
-        echo "Public ssh key has been successfully added to user ${username}"
+        echo "Public ssh key has been successfully added to user ${customer_directory_name}"
     fi
 fi
 
 echo "Please edit security group ${whitelist_ips_sg_id} in order to add whitelisted IPs thus allowing the newly created user to connect to the sftp server"
+
