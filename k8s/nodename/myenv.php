ccd2aa45f4563504ab0d750781365c995c25419c
Itai Ganot <lel@lel.bz>
2024-01-18 16:01:23 +0200

    Adds display nodename service

---
+++ k8s/nodename/myenv.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:powderblue;">
+<head>
+    <title>Kubernetes Node Name</title>
+</head>
+<body>
+    <h1>Kubernetes Node Name</h1>
+    <p><?php echo getenv('NODE_NAME'); ?></p>
+</body>
+</html>
