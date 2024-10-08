c9d82c736b2da8a6e135070ba987937b20f06453
Itai Ganot <lel@lel.bz>
2024-10-08 15:52:04 +0300

    Updates files

---
+++ k8s/k8s_cert_req.sh
@@ -0,0 +1,24 @@
+USER=itaig
+openssl genrsa -out ${USER}.key 2048
+openssl req -new -key ${USER}.key -out ${USER}.csr -subj "/CN=${USER}"
+request=$(cat ${USER}.csr | base64 | tr -d "\n")
+cat <<EOF | kubectl apply -f -
+apiVersion: certificates.k8s.io/v1
+kind: CertificateSigningRequest
+metadata:
+  name: ${USER}
+spec:
+  request: ${request}
+  signerName: kubernetes.io/kube-apiserver-client
+  expirationSeconds: 88886400
+  usages:
+  - client auth
+  - digital signature
+  - key encipherment
+EOF
+kubectl certificate approve ${USER}
+kubectl get csr ${USER} -o jsonpath='{.status.certificate}'| base64 -d > ${USER}.crt
+kubectl create clusterrolebinding ${USER}-binding --clusterrole=cluster-admin --user=${USER}
+kubectl config set-credentials ${USER} --client-key=${USER}.key --client-certificate=${USER}.crt --embed-certs=true
+kubectl config set-context ${USER} --cluster=kubernetes --user=${USER}
+kubectl config use-context ${USER}
