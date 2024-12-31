987481e4b9ab1a765012730900fed397675350c4
Itai Ganot <lel@lel.bz>
2024-12-31 04:11:07 +0200

    Uploads e-commerce-db files

--- e-commerce_app/db/init.sql
+++ e-commerce_app/db/init.sql
@@ -4,16 +4,6 @@ CREATE DATABASE IF NOT EXISTS ${TEMP_MYSQL_DB_NAME};
 -- Step 2: Use the database
 USE ${TEMP_MYSQL_DB_NAME};
 
--- Step 3: Create the application user with a secure password
-CREATE USER '${TEMP_MYSQL_USER}'@'%' IDENTIFIED BY '${TEMP_MYSQL_PASSWORD}';
-ALTER USER '${TEMP_MYSQL_USER}'@'%' IDENTIFIED WITH mysql_native_password BY '${TEMP_MYSQL_PASSWORD}';
-
--- Step 4: Grant full permissions to the application user on the database
-GRANT ALL PRIVILEGES ON ${TEMP_MYSQL_DB_NAME}.* TO '${TEMP_MYSQL_USER}'@'%';
-
--- Step 5: Apply the privilege changes
-FLUSH PRIVILEGES;
-
 -- Step 6: Create the schema (products table)
 CREATE TABLE IF NOT EXISTS ${TEMP_MYSQL_DB_TABLE} (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -28,3 +18,13 @@ INSERT INTO ${TEMP_MYSQL_DB_TABLE} (name, price) VALUES
 ('Product A', 19.99),
 ('Product B', 29.99),
 ('Product C', 39.99);
+
+-- Step 3: Create the application user with a secure password
+--CREATE USER '${TEMP_MYSQL_USER}'@'%' IDENTIFIED BY '${TEMP_MYSQL_PASSWORD}';
+ALTER USER '${TEMP_MYSQL_USER}'@'%' IDENTIFIED WITH 'caching_sha2_password' BY '${TEMP_MYSQL_PASSWORD}';
+
+-- Step 4: Grant full permissions to the application user on the database
+GRANT ALL PRIVILEGES ON ${TEMP_MYSQL_DB_NAME}.* TO '${TEMP_MYSQL_USER}'@'%';
+
+-- Step 5: Apply the privilege changes
+FLUSH PRIVILEGES;
