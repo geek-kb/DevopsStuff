cfc2116ef9a965bc9ec4bf9941c6a0d68bdbd729
Itai Ganot <lel@lel.bz>
2025-02-26 14:04:30 +0200

    Updates repo file

---
+++ Scripts/simple_py_scripts/group_anagrams/group_anagrams.py
@@ -0,0 +1,14 @@
+words = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+def group_anagrams(words):
+    anagrams = {}
+    for word in words:
+        sorted_word = "".join(sorted(word))
+        if sorted_word in anagrams:
+            anagrams[sorted_word].append(word)
+        else:
+            anagrams[sorted_word] = [word]
+    return list(anagrams.values())
+
+print(group_anagrams(words))
+
